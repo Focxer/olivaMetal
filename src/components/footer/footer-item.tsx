7d22339751f653fd82c7ac68ab29252b324e2d5f
Focxer <you@example.com>
2024-10-29 22:55:58 -0300

    add: footer

--- src/components/footer/footer-item.tsx
+++ src/components/footer/footer-item.tsx
@@ -10,18 +10,18 @@ type IFooterItem = {
 
 const FooterItem = (props: IFooterItem) => {
   return (
-    <>
-      <li>
-        <HashLink
-          className="flex gap-2"
-          to={props.link ?? ""}
-          target={props.target ?? "_self"}
-        >
-          <span>{props.icon}</span>
-          <span className="text-zinc-200">{props.text}</span>
-        </HashLink>
-      </li>
-    </>
+    <li className="mb-2">
+      <HashLink
+        className="flex gap-2"
+        to={props.link ?? ""}
+        target={props.target ?? "_self"}
+      >
+        {props.icon && <span>{props.icon}</span>}
+        <span className="text-zinc-200 hover:text-zinc-300 transition-all duration-200">
+          {props.text}
+        </span>
+      </HashLink>
+    </li>
   );
 };
 
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -6,9 +6,9 @@ import { Globals } from "../../globals";
 
 const Footer = () => {
   return (
-    <>
+    <div className="mt-24 w-full">
       <div className="bg-blue-200 h-16 w-full flex justify-between items-center px-8">
-        <h1 className="text-zinc-600 font-semibold text-xl">
+        <h1 className="w-1/2 text-zinc-600 font-semibold text-sm lg:text-xl">
           <span className="text-blue-400">Precisão e Inovação</span> em cada
           detalhe
         </h1>
@@ -33,14 +33,46 @@ const Footer = () => {
           />
         </div>
       </div>
-      <div className="bg-slate-800 bg-opacity-80 w-full py-52 flex items-center justify-around">
-        <ul>
+      <div className="bg-slate-800 bg-opacity-80 w-full px-8 py-24 md:py-36 flex flex-col md:flex-row md:items-center gap-12 justify-around">
+        <ul className="flex justify-center flex-col">
+          <h1 className="text-blue-200 mb-4 font-semibold">Navegação</h1>
           <FooterItem text="Inicio" link={Globals.routes.home}></FooterItem>
+          <FooterItem
+            text="Serviços"
+            link={Globals.routes.services}
+          ></FooterItem>
+          <FooterItem
+            text="Sobre nós"
+            link={Globals.routes.aboutUs}
+          ></FooterItem>
+          <FooterItem text="Contato" link={Globals.routes.contact}></FooterItem>
         </ul>
-        <ul></ul>
-        <ul></ul>
+        <div className="flex flex-col gap-4 text-center text-zinc-200 order-last md:order-none">
+          <Logo color="text-zinc-200 text-left"></Logo>
+          <span>{Globals.customer.address}</span>
+        </div>
+        <ul className="flex justify-center flex-col text-zinc-200 gap-2">
+          <FooterItem
+            icon={<Instagram></Instagram>}
+            text={Globals.customer.instagramLabel}
+            link={Globals.customer.instagramLink}
+          ></FooterItem>
+          <FooterItem
+            icon={<Mail></Mail>}
+            text={Globals.customer.email}
+            link={`mailto:${Globals.customer.email}`}
+          ></FooterItem>
+          <FooterItem
+            icon={<Phone></Phone>}
+            text={Globals.customer.phone}
+            link={`https://wa.me/55${Globals.customer.phone.replaceAll(
+              /[()-]/g,
+              ""
+            )}?text=Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20sobre%20seus%20servi%C3%A7os.`}
+          ></FooterItem>
+        </ul>
       </div>
-    </>
+    </div>
   );
 };
 
